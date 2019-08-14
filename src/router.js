3c775c912518c0a064e4812a45769981259c32bf
Zuray David <intheblackworld@gmail.com>
2019-08-14 12:36:32 +0800

    add fb messenger extension test

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/Home.vue'
+import Register from './pages/Register.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       name: 'home',
       component: Home
     },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
     // {
     //   path: '/formThanks',
     //   name: 'formThanks',
@@ -22,14 +28,5 @@ export default new Router({
     //   component: () =>
     //     import(/* webpackChunkName: "about" */ './pages/FormThanks.vue')
     // },
-    // {
-    //   path: '/phoneThanks',
-    //   name: 'phoneThanks',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ './pages/PhoneThanks.vue')
-    // }
   ]
 })
