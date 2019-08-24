b0d3c34ea1c4e05c1855112fa5147c752e9ef192
Zuray David <intheblackworld@gmail.com>
2019-08-24 14:10:13 +0800

    return history mode

--- src/router.js
+++ src/router.js
@@ -1,23 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/Home.vue'
-import Register from './pages/Register.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register,
     },
     // {
     //   path: '/formThanks',
@@ -28,5 +22,14 @@ export default new Router({
     //   component: () =>
     //     import(/* webpackChunkName: "about" */ './pages/FormThanks.vue')
     // },
+    // {
+    //   path: '/phoneThanks',
+    //   name: 'phoneThanks',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ './pages/PhoneThanks.vue')
+    // }
   ]
 })
