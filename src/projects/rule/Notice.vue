232b31916ef41a0bb02840c07a598c9941e4d7ae
Zuray David <intheblackworld@gmail.com>
2020-01-31 00:28:29 +0800

    新增注意事項

--- src/projects/rule/Notice.vue
+++ src/projects/rule/Notice.vue
@@ -2,16 +2,20 @@
   <div class="dating">
     <div class="title">注意事項</div>
     <p>
-      1.討論好<span class="red">碰面時間，電影院</span>後請點選單
+      1.討論好<span class="red">碰面時間及電影院</span>後請點選單
       <br />
-      <span class="strong">約會快捷功能>敲定約會細節</span>
+      <span class="strong">約會快捷功能>敲定約會細節</span><br />
+      你們有<span class="red">24小時</span>的時間可以討論與交談
+      <br /><span class="red">超過24小時而未敲定約會細節<br />視同取消</span>
     </p>
     <img src="./checkdetail.gif" alt />
+    <br />
+    <br />
     <p>2.聊天都有做備份紀錄，請用一顆禮貌的心跟對方交談</p>
     <p>
       3.約會過程請保持友善態度
       <br />
-      <span class="red">約會結束時會收到通知，此時可以互評對方表現</span>
+      <span class="red">約會結束時會收到通知<br />此時可以互評對方表現</span>
       <br />您的態度決定您的分數喔！
     </p>
     <br />
@@ -21,8 +25,6 @@
     <p>
       如果原定日期喬不攏
       <br />可以跟對方討論彈性修改
-      <br />你們有<span class="red">24小時</span>的時間可以討論與交談
-      <br /><span class="red">過期而未敲定約會細節敲定，視同取消</span>
     </p>
     <div class="end">Let’s Movie 電影約會團隊 祝你約會順利</div>
   </div>
@@ -105,8 +107,8 @@
     min-height: calc(100vh - 40px);
 
     img {
-      width: 100vw;
-      margin-left: -5vw;
+      width: 90vw;
+      margin: 0 auto;
     }
 
     .title {
