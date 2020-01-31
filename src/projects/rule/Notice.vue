2fb7cab5381af100e9e6a3770e8ddc05281d3011
Zuray David <intheblackworld@gmail.com>
2020-01-31 11:32:45 +0800

    注意事項調整

--- src/projects/rule/Notice.vue
+++ src/projects/rule/Notice.vue
@@ -2,25 +2,43 @@
   <div class="dating">
     <div class="title">注意事項</div>
     <p>
-      1.討論好<span class="red">碰面時間及電影院</span>後請點選單
+      1.討論好
+      <span class="red">碰面時間及電影院</span>後請點選單
       <br />
-      <span class="strong">約會快捷功能>敲定約會細節</span><br />
-      你們有<span class="red">24小時</span>的時間可以討論與交談
-      <br /><span class="red">超過24小時而未敲定約會細節<br />視同取消</span>
+      <span class="strong">約會快捷功能>敲定約會細節</span>
+      <br />你們有
+      <span class="red">24小時</span>的時間可以討論與交談
+      <br />
+      <span class="red">
+        超過24小時而未敲定約會細節
+        <br />視同取消
+      </span>
     </p>
     <img src="./checkdetail.gif" alt />
     <br />
     <br />
-    <p>2.聊天都有做備份紀錄，請用一顆禮貌的心跟對方交談</p>
     <p>
-      3.約會過程請保持友善態度
+      2.敲定約會後會關閉對話
+      <br />系統會根據<span class="red">敲定約會細節時輸入的日期</span><br />
+      在<span class="red">約會當天</span>為你們
+      <span class="red">開啟對話</span>，方便碰面
+    </p>
+    <p>3.聊天都有做備份紀錄，請用一顆禮貌的心跟對方交談</p>
+    <p>
+      4.約會過程請保持友善態度
       <br />
-      <span class="red">約會結束時會收到通知<br />此時可以互評對方表現</span>
+      <span class="red">
+        約會結束時會收到通知
+        <br />此時可以互評對方表現
+      </span>
       <br />您的態度決定您的分數喔！
     </p>
     <br />
     <h3>備註一</h3>
-    <p>如果電影場次的時間還沒出來，可以先討論<span class="red">碰面時間</span></p>
+    <p>
+      如果電影場次的時間還沒出來，可以先討論
+      <span class="red">碰面時間</span>
+    </p>
     <h3>備註二</h3>
     <p>
       如果原定日期喬不攏
@@ -133,7 +151,7 @@
     .end {
       font-size: 24px;
       margin-bottom: 30px;
-      color:#333;
+      color: #333;
     }
   }
 }
