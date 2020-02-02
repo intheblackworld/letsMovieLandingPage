d9a0a69d7041d8ffb8cf58fc3c63ceb9706a4918
Zuray David <intheblackworld@gmail.com>
2020-02-02 19:51:03 +0800

    錯誤處理

--- src/projects/webview/CheckDetail.vue
+++ src/projects/webview/CheckDetail.vue
@@ -242,7 +242,6 @@ export default {
                 title: res.err,
               })
               this.loading = false
-              this.closeWebView()
             } else {
               this.loading = false
               this.closeWebView()
