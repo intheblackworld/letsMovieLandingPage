af5e64959a903866e02cd368c2899c605cab4250
Zuray David <intheblackworld@gmail.com>
2020-03-25 20:56:12 +0800

    modify

--- src/info/meta.js
+++ src/info/meta.js
@@ -2,5 +2,5 @@ exports.info = {
   title: 'Lets Movie｜電影約會交友｜女生免費看電影',
   description: '擺脫那些無效社交｜聊天機器人，隱密資訊不外洩｜找一個跟你在電影院裡會心一笑的人',
   keywords: '約會交友，女生免費看電影，',
-  image: '@/public/img/fb_image.png'
+  image: require('@/public/img/fb_image.png')
 }
