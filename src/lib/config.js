581ff9100a34e7147087929160e5839bc13f449e
Zuray David <intheblackworld@gmail.com>
2020-05-24 14:23:49 +0800

    modify

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,20 +1,7 @@
 import info from '../info/index.js'
 
-const fadeOption = (origin, duration, delay, distance) => ({
-  origin,
-  delay,
-  distance: `${distance}px`,
-  duration,
-  viewOffset: {
-    top: -100,
-    right: 0,
-    bottom: 0,
-    left: 0
-  }
-})
-
 export default {
-  install(Vue, options) {
+  install(Vue) {
     Vue.prototype.$gtmCode = info.gtmCode
 
     // Vue.prototype.showFootTab = {
