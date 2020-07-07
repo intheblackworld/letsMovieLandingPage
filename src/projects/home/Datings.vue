2732e610ab8a28e3f9d4f31df80779814894f4a3
Zuray David <intheblackworld@gmail.com>
2020-07-07 19:00:24 +0800

    modify

--- src/projects/home/Datings.vue
+++ src/projects/home/Datings.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="datings relative">
     <div class="title">最新成功約會</div>
-    <swiper
-      :options="swiperOption"
-      ref="mySwiper"
-    >
-      <swiper-slide
+    <div class="animate-slide">
+      <div
+        v-for="(date, index) in datings"
+        :key="date.girl_name + date.boy_name + date.movie_name + date.theater + index"
+        class="dating-item"
+      >
+        <div class="dating-inner">
+          <img
+            :src="date.img_url"
+            alt=""
+          >
+          <div class="dating-content">
+            <p>
+              恭喜
+              <span class="red"> {{date.girl_name}} </span>與<span class="blue"> {{date.boy_name}} </span><br />
+              在<span class="hot"> {{date.meet_time}} </span>成功出去約會<br />
+              🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟<br />
+              他們觀看的電影是: <span class="hot">{{date.movie_name}}</span><br />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
         v-for="(date, index) in datings"
         :key="date.girl_name + date.boy_name + date.movie_name + date.theater + index"
         class="dating-item"
@@ -23,15 +41,10 @@
             他們觀看的電影是: <span class="hot">{{date.movie_name}}</span><br />
           </p>
         </div>
-      </swiper-slide>
-    </swiper>
+      </div>
+    </div>
   </div>
 </template>
-<style lang="scss">
-.el-date-table td.current:not(.disabled) span {
-  color: #fff !important;
-}
-</style>
 <style lang="scss" scoped>
 .datings {
   background: #222126;
@@ -41,7 +54,7 @@
 }
 
 .container {
-  width: 100vw;
+  width: auto;
   margin: 0 auto;
 }
 
@@ -55,15 +68,16 @@
   margin-bottom: 25px;
 }
 .dating-item {
-  // width: 30%;
-  margin: 25px auto;
-  background-color: #1d1c26;
-  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.8);
-  border-radius: 10px;
+  width: 25vw;
+  padding: 25px;
   position: relative;
-  overflow: hidden;
   z-index: 10;
-
+  .dating-inner {
+    border-radius: 10px;
+    overflow: hidden;
+    background-color: #1d1c26;
+    box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.8);
+  }
   .dating-content {
     padding: 20px 15px 15px 20px;
   }
@@ -73,7 +87,7 @@
     height: 350px;
     object-fit: cover;
     object-position: center;
-    border: none
+    background-color: #1d1c26;
   }
 
   p {
@@ -95,50 +109,27 @@
   }
 }
 
-.box {
+.animate-slide {
+  display: flex;
   position: absolute;
-  top: 40vh;
-  transform: rotate(80deg);
-  left: 30vw;
-}
-.wave {
-  top: 0;
+  z-index: 4;
   left: 0;
-  opacity: 0.4;
-  position: absolute;
-  top: 3%;
-  left: 10%;
-  background: linear-gradient(to left, #c31432, #240b36);
-  width: 1500px;
-  height: 1300px;
-  transform-origin: 50% 48%;
-  border-radius: 43%;
-  animation: drift 7000ms infinite linear;
-
-  // &.-two {
-  //   background: #af0404;
-  //   opacity: 0.1;
-  //   width: 300vw;
-  //   height: 180vw;
-  //   animation: drift 10000ms infinite linear;
-  // }
-
-  // &.-three {
-  //   background: #ff0000;
-  //   opacity: 0.05;
-  //   width: 1000px;
-  //   height: 700px;
-  //   animation: drift 13000ms infinite linear;
-  // }
-}
-@keyframes drift {
-  from {
-    transform: rotate(180deg);
-  }
-  from {
-    transform: rotate(360deg);
+  animation: moving 80s linear infinite;
+  &:hover {
+    animation-play-state: paused;
   }
 }
+
+@keyframes moving {
+  0% {
+    transform: translateX(0vw);
+  }
+
+  100% {
+    transform: translateX(-250vw);
+  }
+}
+
 /* 螢幕尺寸標準 */
 /* 平板尺寸 */
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
@@ -146,6 +137,37 @@
 
 /* 手機尺寸 */
 @media only screen and (max-width: 767px) {
+  .datings {
+    background: #222126;
+    color: #fff;
+    min-height: calc(100vw * (720 / 375));
+    padding: 40px 0px 40px 0px;
+  }
+
+  .dating-item {
+    width: 100vw;
+  }
+
+  .animate-slide {
+    display: flex;
+    position: absolute;
+    z-index: 4;
+    left: 0;
+    animation: moving 160s linear infinite;
+    &:hover {
+      animation-play-state: paused;
+    }
+  }
+
+  @keyframes moving {
+    0% {
+      transform: translateX(0vw);
+    }
+
+    100% {
+      transform: translateX(-2000vw);
+    }
+  }
 }
 
 // 避免內容電腦過渡平板時，設計未考量的調整
@@ -161,81 +183,16 @@
 // @ is an alias to /src
 import { isMobile } from '@/utils'
 import moment from 'moment'
-import slider from '@/mixins/slider.js'
-import 'swiper/dist/css/swiper.css'
-
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'datings',
-  mixins: [slider],
-  components: {
-    swiper,
-    swiperSlide,
-  },
 
   data() {
     return {
       me: '',
       moment: moment,
       fb_id: '',
-      swiperOption: {
-        slidesPerView: isMobile ? 1.3 : 4,
-        centeredSlides: true,
-        spaceBetween: isMobile ? 20 : 50,
-        slidesPerColumn: 1,
-        autoplay: {
-          delay: isMobile ? 3000 : 2500,
-        },
-        loop: true,
-      },
-      datings: [
-        {
-          movie_name: '',
-          theater: '',
-          meet_time: '',
-          girl_scores: '',
-          boy_scores: '',
-          girl_name: '',
-          boy_name: '',
-        },
-        {
-          movie_name: '',
-          theater: '',
-          meet_time: '',
-          girl_scores: '',
-          boy_scores: '',
-          girl_name: '',
-          boy_name: '',
-        },
-        {
-          movie_name: '',
-          theater: '',
-          meet_time: '',
-          girl_scores: '',
-          boy_scores: '',
-          girl_name: '',
-          boy_name: '',
-        },
-        {
-          movie_name: '',
-          theater: '',
-          meet_time: '',
-          girl_scores: '',
-          boy_scores: '',
-          girl_name: '',
-          boy_name: '',
-        },
-        {
-          movie_name: '',
-          theater: '',
-          meet_time: '',
-          girl_scores: '',
-          boy_scores: '',
-          girl_name: '',
-          boy_name: '',
-        },
-      ],
+      datings: [],
       // interval: '',
     }
   },
@@ -264,7 +221,6 @@ export default {
           })
         } else {
           this.datings = res.data
-          this.$refs.mySwiper.swiper.update()
         }
       })
   },
